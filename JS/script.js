6e4eeb7941ca342e37e481628619386ea585881b
Saurob17 <mdsaurobbhuyan@gmail.com>
2025-09-16 04:07:26 +0600

    add pic

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+
+// Trggle mobile menu visivility
+menuOpenButton.addEventListener("click", () => {
+  document.body.classList.toggle("show-mobile-menu");
+});
+menuCloseButton.addEventListener("click", () => {
+    document.body.classList.remove("show-mobile-menu");
+});
+
