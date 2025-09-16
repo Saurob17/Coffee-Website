dc747a3f070988e11542ae61b58c6918fc5d65e3
Saurob17 <mdsaurobbhuyan@gmail.com>
2025-09-16 20:52:20 +0600

    2nd_final_

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,40 @@
+// Mobile menu toggle
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
 
-
-// Trggle mobile menu visivility
 menuOpenButton.addEventListener("click", () => {
   document.body.classList.toggle("show-mobile-menu");
 });
 menuCloseButton.addEventListener("click", () => {
-    document.body.classList.remove("show-mobile-menu");
+  document.body.classList.remove("show-mobile-menu");
 });
 
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+  slidesPerView: 1,
+
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets:true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
